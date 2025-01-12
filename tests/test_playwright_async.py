0ba6a1c46a37fbc9738c994bc335b893237580b0
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2025-01-12 21:18:28 +0100

    Setup Verbose Pytests for more Information

--- tests/test_playwright_async.py
+++ tests/test_playwright_async.py
@@ -19,3 +19,15 @@ async def test_async_challenger(async_page: Page):
     with suppress(RecursionError):
         res = await challenger.solve_recaptcha()
         assert res
+
+@pytest.mark.asyncio
+async def test_async_challenger1(async_page: Page):
+    challenger = AsyncChallenger(async_page, click_timeout=1000)
+    # For slow Pytest Loading
+    challenger.detector.detection_models.check_loaded()
+
+    await async_page.goto("https://recaptcha-demo.appspot.com/recaptcha-v2-checkbox-explicit.php")
+
+    with suppress(RecursionError):
+        res = await challenger.solve_recaptcha()
+        assert res
